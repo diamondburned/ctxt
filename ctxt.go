2db38a5d3c12c291dd51ed4d1f8955b7d11f92ec
diamondburned <diamond@libdb.so>
2024-02-29 01:31:53 -0800

    Add `FromOr{,Func}` for convenience

--- ctxt.go
+++ ctxt.go
@@ -19,3 +19,26 @@ func From[T any](ctx context.Context) (T, bool) {
 	v, ok := ctx.Value(key[T]{}).(T)
 	return v, ok
 }
+
+// FromOr returns the value associated with the wanted type or the given default
+// value if the type is not found.
+func FromOr[T any](ctx context.Context, def T) T {
+	v, ok := From[T](ctx)
+	if !ok {
+		return def
+	}
+	return v
+}
+
+// FromOrFunc returns the value associated with the wanted type or the result of
+// the given function if the type is not found.
+// For example:
+//
+//	logger := ctxt.FromOrFunc(ctx, slog.Default)
+func FromOrFunc[T any](ctx context.Context, f func() T) T {
+	v, ok := From[T](ctx)
+	if !ok {
+		return f()
+	}
+	return v
+}
